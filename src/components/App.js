3233d366853f608f682250a51d3ef2677c689a66
Manish baghel <baghelmanish660@gmail.com>
2020-04-22 19:30:07 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import MyButton from './Mat';
-import { ThemeProvider } from '@material-ui/core/styles';
-import theme from './theme';
+import MiniDrawer from './AppHeader';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-        <div className="App">
-            <header className="App-header">
-            <MyButton />
-            <p>
-                Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            el="noopener noreferrer"
-            >
-                Learn React
-            </a>
-            </header>
-        </div>
-    </ThemeProvider>
+    <div className="App">
+        <MiniDrawer />
+    </div>
   );
 }
 
