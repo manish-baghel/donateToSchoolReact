07bdf1ca3c566d3496ececc337aa16051bd90695
Manish baghel <baghelmanish660@gmail.com>
2020-05-29 06:20:27 +0530

    Landing Page sections and Footer component added

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from "react";
-import Banner from "./Banner";
+import Banner, {LandingSection, TeamSection} from "./Banner";
 import "../app.css";
+import Footer from "./Footer";
 
 function App() {
-  return <Banner />;
+  return (
+    <>
+      <Banner />
+      <LandingSection />
+      <LandingSection direction="reverse"/>
+      <LandingSection />
+      <TeamSection />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
