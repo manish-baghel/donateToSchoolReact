1b0ce1099e4b639ed0e594d4f50b100cb65235d7
Manish baghel <baghelmanish660@gmail.com>
2020-07-14 13:50:55 +0530

    heroku config [revert change from BrowserRouter -> Router]

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
+import {Router, Route, Switch, Redirect} from "react-router-dom";
 import {history} from "../configureStore";
 import {connect} from "react-redux";
 import {alertActions} from "../actions/alertActions";
@@ -9,31 +9,32 @@ import Signup from "./Signup";
 import Requirements from "./Requirements";
 import RequirementPage from "./RequirementPage";
 import PrivateRoute from "./PrivateRoute";
+import {history} from "../configureStore";
 
 const Routes = (props) => {
   return (
-    <BrowserRouter  basename={process.env.PUBLIC_URL}>
+    <Router history={history}>
       <Switch>
         <Route exact path="/">
           <Landing />
         </Route>
-        <Route exact path="/login">
+        <Route path="/login">
           <Login />
         </Route>
-        <Route exact path="/signup">
+        <Route path="/signup">
           <Signup />
         </Route>
-        <Route exact path="/requirements">
+        <Route path="/requirements">
           <Requirements category="requirement"/>
         </Route>
-        <Route exact path="/volunteer">
+        <Route path="/volunteer">
           <Requirements category="volunteer"/>
         </Route>
-        <PrivateRoute exact path="/req" currentUser={props.currentUser}>
+        <PrivateRoute path="/req" currentUser={props.currentUser}>
           <RequirementPage />
         </PrivateRoute>
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 };
 
